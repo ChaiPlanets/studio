9dede7a086989236f42e8b2582b7ef5df3197263
Chai Planet <chai.planets@gmail.com>
2025-08-31 11:23:32 +0000

    can you add two more color themes to this application

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Moon, Sun, Palette } from "lucide-react";
+import { useTheme } from "next-themes";
 import {
   Avatar,
   AvatarFallback,
@@ -12,11 +14,17 @@ import {
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
 export function UserNav() {
+  const { setTheme } = useTheme();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -47,6 +55,31 @@ export function UserNav() {
           <DropdownMenuItem>
             Settings
           </DropdownMenuItem>
+           <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <Palette className="mr-2 h-4 w-4" />
+              <span>Theme</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  <Sun className="mr-2 h-4 w-4" />
+                  <span>Light</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  <Moon className="mr-2 h-4 w-4" />
+                  <span>Dark</span>
+                </DropdownMenuItem>
+                 <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => setTheme("oceanic")}>
+                  <span>Oceanic</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("sunset")}>
+                  <span>Sunset</span>
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
