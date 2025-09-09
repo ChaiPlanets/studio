7b3767e5d19ad618582a112d5bd17c7e94489fd0
Chai Planet <chai.planets@gmail.com>
2025-09-09 15:05:09 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
-import type {NextConfig} from 'next';
 
-const nextConfig: NextConfig = {
+const nextConfig: import('next').NextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
